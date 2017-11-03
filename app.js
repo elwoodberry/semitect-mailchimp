5abcaf5081f65202725ba02e19ceaf68dc288cdf
Elwood Berry <me@elwoodberry.com>
2017-11-02 22:18:19 -0400

    Updated Project

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// BODY PARSER MIDDLEWARE
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+// STATIC FOLDER
+app.use(express.static(`${__dirname}/www`));
+
+// INDEX ROUTE
+app.get('/', (req, res) => {
+  res.render('index.html');
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log('Server has started on :' + port);
+});
